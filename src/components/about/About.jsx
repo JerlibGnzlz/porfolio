6e6bea08a1aaa49d0015c2f406bd6cecc7c2d15e
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-05 18:24:55 -0300

    fix about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,3 @@
-
 import "./About.css";
 import foto from "../../media/nueva.png";
 import cv from "../../media/FS.pdf";
@@ -7,36 +6,54 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="about-description">
-        <h3>Te Contare sobre mi.</h3>
-        <p>
-          <strong>
-            Soy un 🇻🇪 apasionado por la informatica radicado en 🇦🇷.
-          </strong>
-     
-          <p>Mis habilidades tecnicas estan orientadas a tecnologias</p>
-          <strong>
-            HTML5 | CSS3 | Javascript | Typescript | Github | React | React Native |
-             NodeJs
-            | Express | Sequelize | Postgres | MongoDb | Scrum | jira | Linux distros debian | Stack: MERN - PERN 🚀.
-          </strong>
-          <br></br><br></br>
-          Entre mis habilidades blandas encontraras: <br></br>Proactividad,
-          Sociabilidad, Colaboracion, Curiosidad y continuo aprendizaje.
-            <div className="cv">
-              <a href={cv} download="CVBklast.pdf">
-                <i className="fa-solid fa-download"></i>
-                Descargar Currículum
-              </a>
-            </div>
-        </p>
-        {/* <strong>
-        </strong> */}
+        <h1>Te Contare sobre mi.</h1>
+
+        <h2>Soy un 🇻🇪 apasionado por la informatica radicado en 🇦🇷.</h2>
+
+        <p>Mis habilidades tecnicas estan orientadas a tecnologias</p>
+
+        <h4>
+          Html5 | Css3 | Javascript | Typescript | Github | React | React
+          Native | Next | NodeJs | Express | Sequelize | Prisma | Postgres | MongoDb | Scrum| Swagger | Render | Vercel | Insomnia REST | Jira | Linux debian.
+
+          <div>
+            <span className="span">
+              <p>
+                Stack:
+                <h2>
+                  MERN - PERN 🚀.
+                </h2>
+              </p>
+            </span>
+          </div>
+        </h4>
+
+        <div className="habilidades">
+          <h3>
+            Entre mis habilidades blandas encontraras: Proactividad,
+            Sociabilidad, Colaboracion, Curiosidad y continuo aprendizaje.
+          </h3>
+        </div>
+      </div>
+
+
+      <div className="cv">
+        <a href={cv} download="CVBklast.pdf">
+          <i className="fa-solid fa-download"></i>
+          Descargar Currículum
+        </a>
+      </div>
+
 
       <div className="about-img">
         <img src={foto} alt="about" />
       </div>
-      </div>
+
     </div>
+
+
+
+
   );
 };
 
